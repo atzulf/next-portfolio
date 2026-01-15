6c947a10c2799a070f9e8f0dc5a083661eabab4a
atzulf <atzulfikar45@gmail.com>
2026-01-15 13:52:34 +0700

    add(Experience): create Experience component and ExperiencePage to display it

---
+++ app/experience/page.tsx
@@ -0,0 +1,11 @@
+import Experience from './Experience';
+import Navbar from '@/components/Navbar';
+
+export default function ExperiencePage() {
+    return (
+        <>
+        <Navbar />
+        <Experience />
+        </>
+    );
+}
