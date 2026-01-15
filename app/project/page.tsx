dd264ebcd1a953dbec6fa09311f9f7c649cb3db8
atzulf <atzulfikar45@gmail.com>
2026-01-15 13:52:08 +0700

    add(Project): create Project component and ProjectPage to display it

---
+++ app/project/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/components/Navbar";
+import Project from "./Project";
+
+export default function ProjectPage() {
+  return (
+    <>
+      <Navbar />
+      <Project />
+    </>
+  );
+}
