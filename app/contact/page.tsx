62384ddd26500bd3191b9d4a4c46d97febf34714
atzulf <atzulfikar45@gmail.com>
2026-01-15 13:52:47 +0700

    add(Contact): create Contact component and ContactPage to display it

---
+++ app/contact/page.tsx
@@ -0,0 +1,11 @@
+import Contact from './Contact';
+import Navbar from '@/components/Navbar';
+
+export default function ContactPage() {
+    return (
+        <>
+        <Navbar />
+        <Contact />
+        </>
+    );
+}
